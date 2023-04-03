1032ffcd0c0aa290a10f3f1100dd0f72a02a5f40
U-BSD\398256 <398256@SHS-DGDRCS2.beaverton.k12.or.us>
2023-04-03 09:18:23 -0700

    display work

--- main.cpp
+++ main.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
+#include <vector>
 #include <cstring>
+#include <fstream>
 #include "Node.h"
 
 using namespace std;
 
-void add_to_tree(Node* value, Node* current);
+void add_to_tree(Node* value, Node* current, Node* &head);
+void display_tree(Node* current, int depth);
 
 int main(){
   char input[20];
-  Node* head = NULL;
+  Node* head = new Node();
   bool running = true;
 
   while(running){
@@ -28,7 +31,7 @@ int main(){
 	cin.ignore(50, '\n');
 
 	vector<int> fileNums;
-	ifstream file(fileName);
+	ifstream file("number.txt");
 	if(file.is_open()){
 	  int a;
 	  while(file >> a){
@@ -43,33 +46,73 @@ int main(){
 	for(ptr = fileNums.begin(); ptr < fileNums.end(); ptr++){//add all values
 	  Node* newNode = new Node();
 	  newNode->setValue(*ptr);
-	  add_to_tree(newNode, head);
+	  add_to_tree(newNode, head, head);
 	}
+      }else if(strcmp(input, "NUMBER") == 0){ //adding by number
+	int number;
+	cout << "What is the number you'd like to add?" << endl;
+	cin >> number;
+	cin.ignore(10, '\n');
+
+	Node* newNode = new Node();
+	newNode->setValue(number);
+	add_to_tree(newNode, head, head);
       }
+    }else if(strcmp(input, "DELETE") == 0){//delete a value in tree
+      int number;
+      cout << "What number would you like to delete?" << endl;
+      cin >> number;
+      cin.ignore(10, '\n');
+
+      //delete_from_tree(number); //FINISH HERE
+      
+    }else if(strcmp(input, "DISPLAY") == 0){//print the tree
+      display_tree(head, 0);
+      
+    }else if(strcmp(input, "SEARCH") == 0){//search for a number in tree
+
     }
   }
   return 0;
 }
 
+
+//prints numbers from the tree in order
+void display_tree(Node* current, int depth){
+  if(current->getLeft() != NULL){ //still a value to the left
+    display_tree(current->getLeft(), (depth+1));
+  }
+  for(int i = 0; i < depth; i++){
+    cout << '\t';
+  }
+  cout << current->getValue() << endl;
+
+  if(current->getRight() != NULL){//still value to the right
+    display_tree(current->getRight(), (depth+1));
+  }
+}
+
+
+
+
 //add number value to tree
-void add_to_tree(Node* value, Node* current){
-  if(current == NULL){//first value
-    current->setValue(value);
-  }else{
+void add_to_tree(Node* value, Node* current, Node* &head){
+  if(head == NULL){//first value
+    current->setValue(value->getValue());
+    head = current;
+  }else{//not first 
     if(current->getValue() > value->getValue()){ //value goes to left
       if(current->getLeft() == NULL){//value goes to immediate left
 	current->setLeft(value);
       }else{ //keep going down left of head
-	add_to_tree(value, current->getLeft());
+	add_to_tree(value, current->getLeft(), head);
       }
     }else if(current->getValue() <= value->getValue()){//value goes to right
       if(current->getRight() == NULL){//value goes to immediate right
 	current->setRight(value);
       }else{//keep going down right of head
-	add_to_tree(value, current->getRight());
+	add_to_tree(value, current->getRight(), head);
       }
-    }
-      
     }
   }
 }
